69d0433fa4ec3b1d59ced72549b8b335ba138262
のど <106727069+doraitoron@users.noreply.github.com>
2024-06-14 20:12:32 +0900

    時報

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function setup(){
-	createCanvas(windowWidth, windowHeight);
-	frameRate(8);
+var sound1;
+function preload(){
+  sound1=loadSound('BGM.mp3');
+  
 }
 
-function draw(){
-	background(33);
-	let date = new Date();
+function setup() {
+  createCanvas(400, 400);
+}
+  var soundOn=true;
+var re;
+
+function draw() {
+    let date = new Date();
+  
   background(220);
   // フォントを変える
   textFont('oswald');
@@ -14,8 +21,8 @@ function draw(){
   textSize(80);
   var m="";
   var s="";
-  var ziho=[16,10,"~",35];
-  
+  var ziho=[20,0,"~",59];
+  // console.log(data)
   if(date.getMinutes()<10){
      m="0";
      }else{
@@ -28,4 +35,23 @@ function draw(){
      }
   text(date.getHours()+":"+m+date.getMinutes()+":"+s+date.getSeconds(),0,200)
   
+  if(!soundOn){
+text("活動終了",0,300)
+  
+  }
+console.log(date.getHours()+":"+date.getMinutes()) // Tue Feb 23 2021 21:56:22 GMT+0900 (日本標準時)
+  
+  if(date.getHours()==ziho[0] && date.getMinutes()>=ziho[1] && date.getMinutes()<=ziho[3]){
+    if(soundOn){
+       sound1.play();
+      soundOn=false;
+      re=date.getSeconds()-1;
+       }
+     
+     }
+  if(date.getSeconds()==re){
+    soundOn=true;
+  }
+
+  // background(220);
 }
